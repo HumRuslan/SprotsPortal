6b8fca235a1e805205233027a70e8288b2c7c6de
Ruslan Huminskiy <rhumi@softserveinc.com>
2021-10-21 16:30:12 +0300

    # SP004. Create seeds to fillup our users

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,17 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+20.times do |n|
+  if n.even?
+    User.create!(
+      first_name: "UserName #{n}",
+      last_name: "UserLastName #{n}",
+      email: "user#{n}@gmail.com",
+      password: "Password@#{n}",
+      password_confirmation: "Password@#{n}"
+    )
+  else
+    User.create!(
+      email: "user#{n}@gmail.com",
+      password: "Password@#{n}",
+      password_confirmation: "Password@#{n}"
+    )
+  end
+end

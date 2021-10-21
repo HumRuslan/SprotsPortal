13e12cc0ba347c786ff357f37915085286ad65c0
Ruslan Huminskiy <rhumi@softserveinc.com>
2021-10-21 16:22:33 +0300

    # SP003. User list page

---
+++ app/models/user.rb
@@ -0,0 +1,14 @@
+class User < ApplicationRecord
+  VALID_PASSWORD = /(?=.*[0-9])(?=.*[!@#$%^&*])(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z!@#$%^&*]{6,}/.freeze
+
+  validates :email, presence: true, format: { with: URI::MailTo::EMAIL_REGEXP,
+                                              message: "invalid email" }
+  validates :password,
+            confirmation: true,
+            presence: true,
+            format: { with: VALID_PASSWORD,
+                      message: "password must contain small, large letters, special characters and numbers," \
+                               " must be more than 5 characters" }
+  validates :first_name, length: { minimum: 2 }, unless: -> { first_name.blank? }
+  validates :last_name, length: { minimum: 2 }, unless: -> { last_name.blank? }
+end

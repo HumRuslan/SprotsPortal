97d8c0eca39859e2d0f8b537c10be2e15b75d8e2
Ruslan Huminskiy <rhumi@softserveinc.com>
2021-10-22 15:49:07 +0300

    Add factory user and test factory user

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_21_105516) do
+ActiveRecord::Schema.define(version: 2021_10_22_090135) do
 
   create_table "users", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "first_name"
@@ -19,6 +19,8 @@ ActiveRecord::Schema.define(version: 2021_10_21_105516) do
     t.string "email"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["email"], name: "index_users_on_email"
+    t.index ["first_name", "last_name"], name: "index_users_on_first_name_and_last_name"
   end
 
 end

15521a01b2c940ecb583a9310cc3745d462370d9
Ruslan Huminskiy <rhumi@softserveinc.com>
2021-10-22 16:05:00 +0300

    Work

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,17 @@
-require 'factory_bot_rails'
-
-20.times do
-  FactoryBot.create :user
+20.times do |n|
+  if n.even?
+    User.create!(
+      first_name: "UserName #{n}",
+      last_name: "UserLastName #{n}",
+      email: "user#{n}@gmail.com",
+      password: "Password@#{n}",
+      password_confirmation: "Password@#{n}"
+    )
+  else
+    User.create!(
+      email: "user#{n}@gmail.com",
+      password: "Password@#{n}",
+      password_confirmation: "Password@#{n}"
+    )
+  end
 end

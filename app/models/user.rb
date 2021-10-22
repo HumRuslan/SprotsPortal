9008cb2115bc18518b1a1aaec127c8d2356080bd
Ruslan Huminskiy <rhumi@softserveinc.com>
2021-10-22 16:07:30 +0300

    Work

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,9 +7,7 @@ class User < ApplicationRecord
   validates :password,
             confirmation: true,
             presence: true,
-            format: { with: VALID_PASSWORD,
-                      message: "password must contain small, large letters, special characters and numbers," \
-                               " must be more than 5 characters" }
+            length: { minimum: 6 }
   validates :first_name, length: { minimum: 2 }, presence: true
   validates :last_name, length: { minimum: 2 }, presence: true
 end

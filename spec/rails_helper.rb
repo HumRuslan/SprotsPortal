babd72347e1357b8089a177c2638d19d442a631f
Ruslan Huminskiy <rhumi@softserveinc.com>
2021-10-26 13:41:55 +0300

    integr travis

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -5,6 +5,7 @@ SimpleCov.start 'rails' do
   add_filter '/spec/' # for rspec
   add_filter '/app/mailers/'
 end
+SimpleCov.minimum_coverage 90
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 require 'spec_helper'
 ENV['RAILS_ENV'] ||= 'test'

f10daeba5837a3159ebeb6013c278997fea4af73
Ruslan Huminskiy <rhumi@softserveinc.com>
2021-10-28 17:22:48 +0300

    fixed mistake

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,8 @@ class User < ApplicationRecord
          :rememberable,
          :confirmable,
          :validatable
-
+  validates :email, uniqueness: { case_sensitive: false }
+  validates :password, presence: true
   validates :first_name, length: { minimum: 2 }, presence: true
   validates :last_name, length: { minimum: 2 }, presence: true
 end

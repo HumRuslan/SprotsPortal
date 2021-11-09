f02a4d1d9173158b959305bde5d9e15a940b0b06
Ruslan Huminskiy <rhumi@softserveinc.com>
2021-11-09 14:18:28 +0200

    SP022. home page for non registered/simple users.

--- spec/controllers/account/user/home_controller_spec.rb
+++ spec/controllers/account/user/home_controller_spec.rb
@@ -6,14 +6,14 @@ RSpec.describe Account::User::HomeController, type: :controller do
   describe "when user visited index page" do
     it "has get index" do
       get :index
-      expect(response).to render_template("index")
+      expect(response).to have_http_status(:ok)
     end
   end
 
   describe "when user preview article" do
     it "has get show" do
       get :show, params: { id: article.id }
-      expect(response).to render_template("show")
+      expect(response).to have_http_status(:ok)
     end
   end
 end

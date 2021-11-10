dbec5f88d5f0021ed5479e38b42d8fa027cc0bab
Ruslan Huminskiy <rhumi@softserveinc.com>
2021-11-10 08:44:57 +0200

    SP022. fixed flash message

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -26,7 +26,7 @@ class ApplicationController < ActionController::Base
   end
 
   def not_found_record
-    flash["alert"] = 'Record not found denied'
+    flash["alert"] = 'Record not found'
     render file: "public/404.html", status: :not_found
   end
 end

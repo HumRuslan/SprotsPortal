44d9259ab439563a9e6fd9827dad83348cc57fc0
HumRuslan <46590860+HumRuslan@users.noreply.github.com>
2021-11-10 15:36:20 +0200

    SP023. Deploy to Heroku (#27)
    * SP023. Deploy to Heroku
    
    * SP023. fixed mistake
    
    Co-authored-by: Ruslan Huminskiy <rhumi@softserveinc.com>

--- Gemfile
+++ Gemfile
@@ -5,8 +5,6 @@ ruby '2.7.0'
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails', branch: 'main'
 gem 'rails', '~> 6.1.4', '>= 6.1.4.1'
-# Use mysql as the database for Active Record
-gem 'mysql2', '~> 0.5'
 # Use Puma as the app server
 gem 'puma', '~> 5.0'
 # Use SCSS for stylesheets
@@ -28,6 +26,10 @@ gem 'jbuilder', '~> 2.7'
 # Reduces boot times through caching; required in config/boot.rb
 gem 'bootsnap', '>= 1.4.4', require: false
 
+group :production do
+  gem 'pg'
+end
+
 group :development, :test do
   gem 'rails_best_practices'
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
@@ -39,6 +41,8 @@ group :development, :test do
   gem 'rubocop', '~> 1.22'
   gem 'rubocop-rails'
   gem 'rubocop-rspec'
+  # Use mysql as the database for Active Record
+  gem 'mysql2', '~> 0.5'
 end
 
 group :development do
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -46,7 +46,7 @@ Rails.application.configure do
   # config.action_cable.allowed_request_origins = [ 'http://example.com', /http:\/\/example.*/ ]
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
-  # config.force_ssl = true
+  config.force_ssl = true
 
   # Include generic and useful information about system operation, but avoid logging too much
   # information to avoid inadvertent exposure of personally identifiable information (PII).
@@ -63,6 +63,18 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "SportsPortal_production"
 
   config.action_mailer.perform_caching = false
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default_url_options = { host: 'whispering-meadow-70531.herokuapp.com' }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address: 'smtp.gmail.com',
+    port: 587,
+    domain: 'gmail.com',
+    user_name: Rails.application.credentials.email[:user_name],
+    password: Rails.application.credentials.email[:password],
+    authentication: 'plain',
+    enable_starttls_auto: true
+  }
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.

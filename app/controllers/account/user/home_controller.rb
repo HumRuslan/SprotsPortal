cacd419d23d30cd6f7e2bf77a80aa944186887f7
Ruslan Huminskiy <rhumi@softserveinc.com>
2021-11-16 15:13:21 +0200

    SP025. Articles search using elasticsearch. Chewy gem (add search article user)

--- app/controllers/account/user/home_controller.rb
+++ app/controllers/account/user/home_controller.rb
@@ -2,6 +2,7 @@ class Account::User::HomeController < Account::User::UserApplicationController
   skip_before_action :authenticate_user!, only: %i[index show]
   def index
     @articles = Article.all.published
+    @articles_search = @search.search.load(only: 'article.published').objects.select(&:published?)
   end
 
   def show

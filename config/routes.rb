6662c6f969dbfcb8258a56af948608e9581b9da6
HumRuslan <46590860+HumRuslan@users.noreply.github.com>
2021-11-25 10:23:29 +0200

    SP031. I18n for admins/users list table (#41)
    * SP031. I18n for admins/users list table
    
    * SP031. I18n for admins/users list table
    
    Co-authored-by: Ruslan Huminskiy <rhumi@softserveinc.com>

--- config/routes.rb
+++ config/routes.rb
@@ -1,47 +1,49 @@
 Rails.application.routes.draw do
   mount Ckeditor::Engine => '/ckeditor'
 
-  devise_scope :user do
-    root "account/user/home#index"
-  end
-
-  devise_for :users, controllers: {
-    registrations: 'auth/registrations',
-    sessions: 'auth/sessions'
-  }
-
-  namespace :account do
-    namespace :admin do
-      root "home#index"
-      resources :user, only: %i[index destroy] do
-        member do
-          put "blocked"
-          put "activated"
-          put "add-admin"
-          put "remove-admin"
-        end
-      end
-      resources :article do
-        member do
-          put "published"
-          put "unpublished"
-        end
-      end
-      resources :category, only: %i[index new create edit update destroy] do
-        resources :sub_category, only: %i[new create edit update destroy] do
-          resources :team, only: %i[new create edit update destroy]
-        end
-      end
+  scope '(:locale)' do
+    devise_scope :user do
+      root "account/user/home#index"
     end
 
-    namespace :user do
-      root "home#index"
-      resources :article, controller: :home, only: %i[index show] do
-        collection do
-          get 'search'
+    devise_for :users, controllers: {
+      registrations: 'auth/registrations',
+      sessions: 'auth/sessions'
+    }
+
+    namespace :account do
+      namespace :admin do
+        root "home#index"
+        resources :user, only: %i[index destroy] do
+          member do
+            put "blocked"
+            put "activated"
+            put "add-admin"
+            put "remove-admin"
+          end
+        end
+        resources :article do
+          member do
+            put "published"
+            put "unpublished"
+          end
+        end
+        resources :category, only: %i[index new create edit update destroy] do
+          resources :sub_category, only: %i[new create edit update destroy] do
+            resources :team, only: %i[new create edit update destroy]
+          end
         end
       end
-      resources :user, controller: :profile, only: %i[edit update]
+
+      namespace :user do
+        root "home#index"
+        resources :article, controller: :home, only: %i[index show] do
+          collection do
+            get 'search'
+          end
+        end
+        resources :user, controller: :profile, only: %i[edit update]
+      end
     end
   end
 end
